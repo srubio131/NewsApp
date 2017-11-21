25987cc1f7b87d48e8225691351c39a5cff4f9f5
Sergio R <s.rubio131@gmail.com>
2017-11-21 21:38:22 +0100

    Cambiado layout news.html

--- src/js/controllers/mainController.js
+++ src/js/controllers/mainController.js
@@ -2,6 +2,7 @@ var app = angular.module("NewsApp");
 
 app.controller("MainController",["NewsAPIUrlsFactory","$scope","$resource", function(NewsAPIUrlsFactory,$scope,$resource){
 
+    // Cargar últimas noticias
     var urlTopHeadLiness = NewsAPIUrlsFactory.getUrlTopHeadlines('el-mundo');
     $scope.topheadlines = $resource(urlTopHeadLiness, {}).get();
 

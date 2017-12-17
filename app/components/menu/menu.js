93fcb4298a9f3c4e31aa73d4dd4d1662e69719e9
Sergio R <s.rubio131@gmail.com>
2017-12-17 17:50:03 +0100

    Menu

--- app/components/menu/menu.js
+++ app/components/menu/menu.js
@@ -1,24 +1,47 @@
 (function($) {
     $(document).ready(function(){
 
-        // var navbarHeight  = $('.navbar').outerHeight();
-        // var lastScrollTop = 0;
-        //
-        // console.log('nav: '+navbarHeight);
-        //
-        // // Ocultar navbar y al hacer scroll hacerlo visible
-        // $(window).scroll(function (e) {
-        //     var scroll = $(window).scrollTop();
-        //     var currentScroll = $(this).scrollTop();
-        //     if ( scroll >= navbarHeight && currentScroll > lastScrollTop ) {
-        //         //console.log('<100 '+$(window).scrollTop());
-        //         $('.navbar').fadeOut();
-        //     } else {
-        //         //console.log('else '+$(window).scrollTop());
-        //         $('.navbar').fadeIn();
-        //     }
-        //     lastScrollTop = currentScroll;
-        // });
+        // Hide Header on on scroll down
+        var didScroll;
+        var lastScrollTop = 0;
+        var delta = 5;
+        var navbarHeight = $('.navbar').outerHeight();
+
+        $(window).scroll(function(event){
+            didScroll = true;
+        });
+
+        setInterval(function() {
+            if (didScroll) {
+                hasScrolled();
+                didScroll = false;
+            }
+        }, 250);
+
+        function hasScrolled() {
+            var st = $(this).scrollTop();
+
+            // Make sure they scroll more than delta
+            if(Math.abs(lastScrollTop - st) <= delta) {
+                return;
+            }
+
+            // If they scrolled down and are past the navbar, add class .nav-up.
+            // This is necessary so you never see what is "behind" the navbar.
+            var navbarHeight2 = $('.navbar').outerHeight();
+            if (st > lastScrollTop && st > navbarHeight){
+                // Scroll Down
+                $('.navbar').css("top", -navbarHeight2);
+            } else {
+                // Scroll Up
+                //if(st + $(window).height() < $(document).height()) {
+                if(st <= navbarHeight2) {
+                    $('.navbar').css("top", 0);
+                }
+            }
+
+            lastScrollTop = st;
+        }
 
     }) ;
 })(jQuery);
